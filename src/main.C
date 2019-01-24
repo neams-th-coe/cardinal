4e88b9de0e75479008d39c7fc68ec513438fe24a
Ronald Rahaman <rahaman@anl.gov>
2019-01-24 14:35:34 -0600

    Runs OpenMC app from main (but openmc app itself still wrong)

--- src/main.C
+++ src/main.C
@@ -6,29 +6,46 @@
 //*
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
-#include "cardinalTestApp.h"
+//#include "cardinalTestApp.h"
 #include "MooseInit.h"
 #include "Moose.h"
 #include "MooseApp.h"
 #include "AppFactory.h"
+#include "CommandLine.h"
+#include "ExternalOpenmcProblemApp.h"
 
 // Create a performance log
 PerfLog Moose::perf_log("cardinal");
 
 // Begin the main program.
 int
-main(int argc, char * argv[])
+main(int argc, char *argv[])
 {
   // Initialize MPI, solvers and MOOSE
   MooseInit init(argc, argv);
 
-  // Register this application's MooseApp and any it depends on
-  cardinalTestApp::registerApps();
+  // Get which app to run
+  CommandLine::Option app_opt = {"which app to run", {"--app", "app_name"}, true, CommandLine::ARGUMENT::REQUIRED, {}};
 
-  // Create an instance of the application and store it in a smart pointer for easy cleanup
-  std::shared_ptr<MooseApp> app = AppFactory::createAppShared("cardinalTestApp", argc, argv);
+  auto cmds = CommandLine(argc, argv);
+  cmds.addOption("which_app", app_opt);
 
-  // Execute the application
+  std::string which_app;
+  cmds.search("which_app", which_app);
+
+  std::string app_class_name;
+  if (which_app == "openmc") {
+    app_class_name = "ExternalOpenmcProblemApp";
+    ExternalOpenmcProblemApp::registerApps();
+  }
+  else if (which_app == "nek5000" or which_app == "nek") {
+    std::cerr << "--app nek not done yet!" << std::endl;
+  }
+  else
+    std::cerr << "Wrong --app option!" << std::endl;
+
+  // Create and run the app
+  std::shared_ptr<MooseApp> app = AppFactory::createAppShared(app_class_name, argc, argv);
   app->run();
 
   return 0;
