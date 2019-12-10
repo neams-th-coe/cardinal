067dbcc6ae0061a3a6672769b922f0abfb39cbf7
Ronald Rahaman <rahaman@cs.uchicago.edu>
2019-12-10 13:07:47 -0600

    Removed libgs dependency from libcardinal

--- config/nekrs.mk
+++ config/nekrs.mk
@@ -3,9 +3,9 @@ export NEKRS_OBJDIR := $(CURDIR)/obj_nekrs
 export NEKRS_LIBDIR := $(NEKRS_INSTALL_DIR)/lib
 export NEKRS_LIB := $(NEKRS_LIBDIR)/libnekrs.so
 
-export NEKRS_CC := $(CC)
-export NEKRS_CXX := $(CXX)
-export NEKRS_FC := $(FC)
+export NEKRS_CC := $(libmesh_CC)
+export NEKRS_CXX := $(libmesh_CXX)
+export NEKRS_FC := $(libmesh_FC)
 #export NEKRS_CXXFLAGS := $(CXXFLAGS) -DUSE_OCCA_MEM_BYTE_ALIGN=32
 export NEKRS_CXXFLAGS := -O2 -g -DUSE_OCCA_MEM_BYTE_ALIGN=32
 
