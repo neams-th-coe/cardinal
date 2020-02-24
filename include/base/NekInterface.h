a23876d58d356c55ebb636cc24763340e332e86b
Ron Rahaman <rahaman@mcs.anl.gov>
2020-02-24 12:21:17 -0600

    Reverted some of NekInterface.h to avoid merge conflicts

--- include/base/NekInterface.h
+++ include/base/NekInterface.h
@@ -89,9 +89,8 @@ void FORTRAN_CALL(nek_step)();              // call for each picard iteration wi
 void FORTRAN_CALL(nek_finalize_step)();     // call at the end of step
 void FORTRAN_CALL(nek_interpolation)();  // to be called before extracting data from common blocks
 void FORTRAN_CALL(nek_pointscloud)();    // to be called before extracting point cloud locations from common blocks
+void FORTRAN_CALL(flux_reconstruction)(); // to be called after loading flux data in common blocks
 }
-
-double *flux_recon = reinterpret_cast<double *>(nekrs::nekPtr("flux_recon"));
 }
 */
 
