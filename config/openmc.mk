089158a1c0ff7d926cfd8bb49c8e330bc743a702
Patrick Shriwise <pshriwise@gmail.com>
2020-05-01 11:56:48 -0500

    HACK: Creating the 'contrib' dir under the libmesh include directory to avoid CMake error.

--- config/openmc.mk
+++ config/openmc.mk
@@ -4,6 +4,7 @@ OPENMC_LIB := $(OPENMC_LIBDIR)/libopenmc.so
 
 $(OPENMC_LIB):
 	mkdir -p $(OPENMC_OBJDIR)
+	mkdir -p $(LIBMESH_DIR)/include/contrib
 	cd $(OPENMC_OBJDIR) && \
 	PKG_CONFIG_PATH=$(LIBMESH_DIR)/lib/pkgconfig/ \
 	cmake -Dlibmesh=ON -DCMAKE_EXE_LINKER_FLAGS="$(libmesh_LIBS)" -DCMAKE_SHARED_LINKER_FLAGS="$(libmesh_LIBS)" -DCMAKE_INSTALL_PREFIX=$(CURDIR) $(OPENMC_DIR)
