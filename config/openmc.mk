c5706729fb6fff5a761a2bfacf693614d9e31ec6
Patrick Shriwise <pshriwise@gmail.com>
2020-05-07 10:11:57 -0500

    Removing environment variable for libmesh. Have to run cmake twice now, but overall I'd say its an improvement.

--- config/openmc.mk
+++ config/openmc.mk
@@ -6,9 +6,8 @@ $(OPENMC_LIB):
 	mkdir -p $(OPENMC_OBJDIR)
 	mkdir -p $(LIBMESH_DIR)/include/contrib
 	cd $(OPENMC_OBJDIR) && \
-	PKG_CONFIG_PATH=$(LIBMESH_DIR)/lib/pkgconfig/ \
-	cmake -Dlibmesh=ON -DCMAKE_EXE_LINKER_FLAGS="$(libmesh_LIBS)" -DCMAKE_SHARED_LINKER_FLAGS="$(libmesh_LIBS)" -DCMAKE_INSTALL_PREFIX=$(CURDIR) $(OPENMC_DIR)
-	make VERBOSE=1 -C $(OPENMC_OBJDIR) install
+	cmake -L -Dlibmesh=ON -Doptimize=ON -DLIBMESH_DIR=$(LIBMESH_DIR) -DCMAKE_INSTALL_PREFIX=$(CURDIR) $(OPENMC_DIR) && \
+	cmake $(OPENMC_DIR) && make VERBOSE=1 -C $(OPENMC_OBJDIR) install
 
 clean_openmc:
 	make -C $(OPENMC_OBJDIR) clean
