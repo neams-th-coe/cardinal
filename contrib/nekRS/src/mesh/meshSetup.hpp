8aaa9e6df20263a08f7379869b228c2c306eeb9c
Ron Rahaman <rahaman@mcs.anl.gov>
2020-10-29 10:27:50 -0500

    Added nekRS submodule

--- contrib/nekRS/src/mesh/meshSetup.hpp
+++
@@ -1,9 +0,0 @@
-#if !defined(nekrs_meshsetup_hpp_)
-#define nekrs_meshsetup_hpp_
-
-#include "nekrs.hpp"
-mesh_t *createMeshDummy(MPI_Comm comm, int N, setupAide &options, occa::properties &kernelInfo);
-mesh_t *createMeshT(MPI_Comm comm, int N, int isMeshT, setupAide &options, occa::properties &kernelInfo);
-mesh_t *createMeshV(MPI_Comm comm, int N, mesh_t *meshT, setupAide &options, occa::properties &kernelInfo);
-
-#endif
