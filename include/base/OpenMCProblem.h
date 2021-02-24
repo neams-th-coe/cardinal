a7260dd0c1dc1baa66d61cd60b05522538889588
aprilnovak <novak@berkeley.edu>
2021-02-24 16:13:00 -0600

    Allow a single volume to be specified for OpenMC.

--- include/base/OpenMCProblem.h
+++ include/base/OpenMCProblem.h
@@ -44,19 +44,29 @@ public:
   std::vector<double> meshHeatSource();
   double getCellVolume(int cellIndex);
 
-private:
+protected:
+  /// coordinate level of the pebble cells in the OpenMC model
+  const int & _pebble_cell_level;
+
+  /// Total power for normalizing the heat source
+  const Real & _power;
+
+  /// Cell volumes at the locations of the pebble centers
+  std::vector<Real> _volumes;
+
+  /// Tally type used in OpenMC
+  const tally::TallyTypeEnum _tallyType;
+
+  /// Conversion factor from electron volts to Joules
   const double JOULE_PER_EV {1.6021766208e-19};
 
+  /// Spatial dimension of the Monte Carlo problem
   const int DIMENSION {3};
 
   unsigned int _heat_source_var; //! heat source variable number
-  int _pebble_cell_level; //! coordinate level of the pebble cells in the OpenMC model
 
   std::vector<Point> _centers;       //! Locations of the pebble centers
-  Real _power;                       //! Total power produced in the problem (used for heating normalization)
-  std::vector<Real> _volumes;        //! Cell volumes at the location of the pebble centers
   std::string _meshTemplateFilename; //! Filename of the mesh template to use in the unstructured mesh tally
-  const tally::TallyTypeEnum _tallyType;              //! Tally type used in the OpenMC problem (CELL, MESH)
 
   std::vector<int32_t> _cellIndices {};   //! OpenMC cell indices corresponding to the pebble centers
   std::vector<int32_t> _cellInstances {}; //! OpenMC cell instances corresponding to the pebble centers
