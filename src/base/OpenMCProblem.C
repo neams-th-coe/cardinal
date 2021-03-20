1f80bf432ccc5a390661d4c900d6a068ff221a94
Patrick Shriwise <pshriwise@gmail.com>
2021-03-20 00:21:05 -0500

    Using Moose's stringify method instead of to_string.

--- src/base/OpenMCProblem.C
+++ src/base/OpenMCProblem.C
@@ -419,7 +419,7 @@ void OpenMCProblem::checkTallySum(double tally_sum) const {
   double kf_total = kappa_fission_total();
   if ( std::abs(kf_total - tally_sum) / kf_total > openmc::FP_REL_PRECISION) {
     mooseWarning("Heating tally kappa-fission does not match the global kappa-fission value.\n"
-                 "Global value: " + std::to_string(kf_total) + "\nTally sum: " + std::to_string(tally_sum));
+                 "Global value: " + Moose::stringify(kf_total) + "\nTally sum: " + Moose::stringify(tally_sum));
   }
 }
 
