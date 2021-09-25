2ff7de65229cd808228241cbd1d7d78d08599fa1
aprilnovak <novak@berkeley.edu>
2021-09-25 09:28:54 -0500

    Add check for temperature existence for Pe.

--- src/postprocessors/PecletNumber.C
+++ src/postprocessors/PecletNumber.C
@@ -16,6 +16,13 @@ PecletNumber::validParams()
 PecletNumber::PecletNumber(const InputParameters & parameters) :
   ReynoldsNumber(parameters)
 {
+  // rhoCp and k don't get initialized if the temperature solve is turned off,
+  // even if the temperature variable exists. Because the user could technically still
+  // be setting these parameters manually in the .udf file (which we do ourselves
+  // for testing), just throw an error if temperature doesn't exist at all
+  if (!nekrs::hasTemperatureVariable())
+    mooseError("The " + name() + " postprocessor cannot be used in NekRS problems "
+      "without a temperature variable!");
 }
 
 Real
