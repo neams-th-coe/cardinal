edf726a7fe491aa19b087f19edd4787c23d1ac60
aprilnovak <novak@berkeley.edu>
2021-11-11 15:58:53 -0600

    Add auxiliary kernel for extracting componentwise velocity glyphs. Refs #253

---
+++ src/auxkernels/NekSpatialBinComponentAux.C
@@ -0,0 +1,43 @@
+#include "NekSpatialBinComponentAux.h"
+#include "CardinalEnums.h"
+
+registerMooseObject("CardinalApp", NekSpatialBinComponentAux);
+
+defineLegacyParams(NekSpatialBinComponentAux);
+
+InputParameters
+NekSpatialBinComponentAux::validParams()
+{
+  InputParameters params = SpatialUserObjectAux::validParams();
+  params.addRequiredRangeCheckedParam<unsigned int>("component", "component < 3", "Component of user object");
+  params.addClassDescription("Populates an auxiliary variable with a component-wise spatial value returned from a "
+                             "UserObject spatialValue method.");
+  return params;
+}
+
+NekSpatialBinComponentAux::NekSpatialBinComponentAux(const InputParameters & parameters)
+  : SpatialUserObjectAux(parameters),
+    _component(getParam<unsigned int>("component"))
+{
+  // by requiring this userobject, we automatically ensure correct NekRSProblemBase-type problems
+  // because the NekUserObject checks for compatibility
+  _bin_uo = dynamic_cast<const NekSpatialBinUserObject *>(&_user_object);
+
+  if (!_bin_uo)
+    mooseError("This auxkernel can only be combined with NekSpatialBinUserObject-derived classes!\n"
+      "You have specified the '" + _user_object.type() + "' user object instead.\n\n"
+      "Options: NekBinnedSideIntegral, NekBinnedSideAverage, NekBinnedVolumeIntegral, NekBinnedVolumeAverage");
+
+  if (_bin_uo->field() != field::velocity_component)
+    mooseError("This auxkernel can only be used with a binning user object that sets "
+      "'field = velocity_component'!");
+}
+
+Real
+NekSpatialBinComponentAux::computeValue()
+{
+  if (isNodal())
+    return _bin_uo->spatialValue(*_current_node, _component);
+  else
+    return _bin_uo->spatialValue(_current_elem->vertex_average(), _component);
+}
