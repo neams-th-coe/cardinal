9ebe6e3437bb2c491b51daf3438ce3e9c6c2db67
aprilnovak <novak@berkeley.edu>
2022-08-05 16:55:35 -0500

    Add error message to protect against distribtued meshes with tallying on the Mesh

--- src/base/OpenMCCellAverageProblem.C
+++ src/base/OpenMCCellAverageProblem.C
@@ -1817,7 +1817,16 @@ OpenMCCellAverageProblem::initializeTallies()
 
       std::unique_ptr<openmc::LibMesh> tally_mesh;
       if (_tally_mesh_from_moose)
+      {
+        // for distributed meshes, each rank only owns a portion of the mesh information, but
+        // OpenMC wants the entire mesh to be available on every rank. We might be able to add
+        // this feature in the future, but will need to investigate
+        if (!_mesh.getMesh().is_replicated())
+          mooseError("Directly tallying on the [Mesh] block by OpenMC is not yet supported "
+            "for distributed meshes!");
+
         tally_mesh = std::make_unique<openmc::LibMesh>(_mesh.getMesh(), _scaling);
+      }
       else
         tally_mesh = std::make_unique<openmc::LibMesh>(_mesh_template_filename, _scaling);
 
