bae845a6858dd48e233e14b04202a3513afd2eb7
Ansh Chaube <achaube2@illinois.edu>
2023-01-23 13:47:25 -0600

    addresses PR feedback

--- src/base/NekRSProblem.C
+++ src/base/NekRSProblem.C
@@ -242,7 +242,7 @@ NekRSProblem::initialSetup()
                "displacement values at a boundary of interest to calcualte the mesh velocity. This\n"
                "mesh velocity is applied within nekRS on the same boundary to solve for fluid flow\n"
                "in a moving mesh domain using the elasticity solver. If you intend to use the elasticity\n"
-               "solver, please indicate at least one boundary of interest in your Cardinal sub-app\n"
+               "solver, please indicate at least one 'boundary' of interest in your Cardinal sub-app\n"
                "input file's [Mesh] block, regardless of whether you are using a volume mirror or not.");
 
   // For volume-based coupling, we should check that there is a udf function providing
