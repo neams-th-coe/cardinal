a8407032ce94ea6956ef8bfa46b2bf8219084640
Logan Harbour <loganhharbour@gmail.com>
2023-11-22 15:15:26 -0700

    Clean up version logic

--- scripts/download-openmc-cross-sections.sh
+++ scripts/download-openmc-cross-sections.sh
@@ -2,15 +2,16 @@
 set -e
 
 SCRIPT_DIR=$( dirname $(dirname $(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )))
-DOWNLOAD_ROOT=${1:-$(realpath $SCRIPT_DIR/..)/cross_sections}
-DOWNLOAD_DIR=${DOWNLOAD_ROOT}/cross_sections
+DOWNLOAD_DIR=${1:-$(realpath $SCRIPT_DIR/..)/cross_sections}
+XS_VERSION=endfb-vii.1-hdf5
+XS_DIR=${DOWNLOAD_DIR}/${XS_VERSION}
 
-if [[ ! -d ${DOWNLOAD_DIR} ]]; then
-  echo "Downloading cross sections to ${DOWNLOAD_DIR}"
-  mkdir ${DOWNLOAD_DIR}
+if [[ ! -d ${XS_DIR} ]]; then
+  mkdir -p ${XS_DIR}
+  echo "Downloading cross sections to ${XS_DIR}"
   wget -q -O - https://anl.box.com/shared/static/9igk353zpy8fn9ttvtrqgzvw1vtejoz6.xz | tar -C ${DOWNLOAD_DIR} -xJ
 else
-  echo "Skipping cross section download because ${DOWNLOAD_DIR} already exists."
+  echo "Skipping cross section download because ${XS_DIR} already exists."
 fi
 
 set +ex
@@ -19,6 +20,6 @@ if [[ -z "${OPENMC_CROSS_SECTIONS}" ]]; then
   echo ""
   echo "You must now set:"
   echo ""
-  echo "export OPENMC_CROSS_SECTIONS=${DOWNLOAD_DIR}/endfb-vii.1-hdf5/cross_sections.xml"
+  echo "export OPENMC_CROSS_SECTIONS=${XS_DIR}/cross_sections.xml"
   echo ""
 fi
