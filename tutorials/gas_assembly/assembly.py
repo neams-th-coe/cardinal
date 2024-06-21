1e5d6bd4cc220266de7b4128fb356085a091a86a
nuclearkevin <66632997+nuclearkevin@users.noreply.github.com>
2024-06-20 20:29:55 -0400

    Added a seed to the TRISO tutorials Made them deterministic

--- tutorials/gas_assembly/assembly.py
+++ tutorials/gas_assembly/assembly.py
@@ -230,7 +230,7 @@ def assembly(n_ax_zones, n_inactive, n_active, add_entropy_mesh=False):
     # region in which TRISOs are generated
     r_triso = -fuel_cyl & +min_z & -max_z
 
-    rand_spheres = openmc.model.pack_spheres(radius=radius_pyc_outer, region=r_triso, pf=specs.triso_pf)
+    rand_spheres = openmc.model.pack_spheres(radius=radius_pyc_outer, region=r_triso, pf=specs.triso_pf, seed=1.0)
     random_trisos = [openmc.model.TRISO(radius_pyc_outer, u_triso, i) for i in rand_spheres]
 
     llc, urc = r_triso.bounding_box
