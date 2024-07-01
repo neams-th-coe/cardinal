003e4a9282b3381cffd0a6b9bc405ae5f04274bc
Patrick Shriwise <pshriwise@gmail.com>
2024-07-01 11:49:35 -0500

    push for logan

--- src/actions/OpenMCTallyGeneratorAction.C
+++ src/actions/OpenMCTallyGeneratorAction.C
@@ -18,52 +18,53 @@
 
 #ifdef ENABLE_OPENMC_COUPLING
 
-#include "OpenMCTallyGenerator.h"
+#include "OpenMCTallyGeneratorAction.h"
 #include "OpenMCTallyNuclides.h"
 #include "OpenMCProblemBase.h"
 #include "openmc/tallies/tally.h"
 
-registerMooseObject("CardinalApp", OpenMCTallyGenerator);
+registerMooseAction("CardinalApp", OpenMCTallyGeneratorAction, "add_tally_generator");
 
 InputParameters
-OpenMCTallyGenerator::validParams()
+OpenMCTallyGeneratorAction::validParams()
 {
   InputParameters params = GeneralUserObject::validParams();
   params.addRequiredParam<std::vector<std::string>>("ids", "Tally IDs to create in OpenMC");
-  params.addRequiredParam<bool>("nuclides", "Whether or not to create a UserObject to modify nuclides in the new OpenMC tally");
-  params.addClassDescription("An OpenMC tally generation UserObject");
+  params.addRequiredParam<bool>("nuclides", "Whether or not to create a UserObject to modify nuclides in the new OpenMC tallies");
+  params.addClassDescription("An OpenMC tally generation action");
   return params;
 }
 
-OpenMCTallyGenerator::OpenMCTallyGenerator(const InputParameters & parameters)
-  : GeneralUserObject(parameters), _ids(getParam<std::vector<std::string>>("ids")), _nuclides_uo(getParam<bool>("nuclides"))
+OpenMCTallyGeneratorAction::OpenMCTallyGeneratorAction(const InputParameters & parameters)
+  : Action(parameters), _ids(getParam<std::vector<std::string>>("ids")), _nuclides_uo(getParam<bool>("nuclides"))
 {
-  const OpenMCProblemBase * openmc_problem = dynamic_cast<const OpenMCProblemBase *>(&_fe_problem);
-  if (!openmc_problem)
-  {
-    std::string extra_help = _fe_problem.type() == "FEProblem" ? " (the default)" : "";
-    mooseError("This user object can only be used with wrapped OpenMC cases! "
-               "You need to change the\nproblem type from '" +
-               _fe_problem.type() + "'" + extra_help + " to OpenMCCellAverageProblem.");
-  }
+  const OpenMCProblemBase * openmc_problem = dynamic_cast<const OpenMCProblemBase *>(_problem.get());
+  // if (!openmc_problem)
+  // {
+  //   std::string extra_help = _fe_problem.type() == "FEProblem" ? " (the default)" : "";
+  //   mooseError("This user object can only be used with wrapped OpenMC cases! "
+  //              "You need to change the\nproblem type from '" +
+  //              _fe_problem.type() + "'" + extra_help + " to OpenMCCellAverageProblem.");
+  // }
 
 }
 
 void
-OpenMCTallyGenerator::execute()
+OpenMCTallyGeneratorAction::act()
 {
   for (const auto & id : _ids)
   {
     openmc::Tally* tally = openmc::Tally::create(std::stoi(id));
-
+    mooseInfo("Created Tally ID:" + std::to_string(tally->id()));
     if (_nuclides_uo)
     {
-      InputParameters params = OpenMCTallyNuclides::validParams();
-      params.set<int32_t>("tally_id", tally->id());
-      params.set<std::vector<std::string>>("names", {});
-      mooseWarning("Creating OpenMCTallyNuclides for tally with ID " + id);
-      _fe_problem.addUserObject("OpenMCTallyNuclides", "tally_" + id + "_nuclides", params);
-
+      const std::string uo_type = "OpenMCTallyNuclides";
+      InputParameters params = _factory.getValidParams(uo_type);
+      int32_t tally_id = std::stoi(id);
+      params.set<int32_t>("tally_id") = tally_id;
+      params.set<std::vector<std::string>>("names") =  {};
+      mooseWarning("Creating OpenMCTallyNuclides tally_" + id + "_nuclides for tally with ID " + id);
+      _problem->addUserObject(uo_type, "tally_" + id + "_nuclides", params);
     }
   }
   _ids.clear();
--- test/tests/neutronics/openmc_xml/openmc.i
+++ test/tests/neutronics/openmc_xml/openmc.i
@@ -11,7 +11,12 @@
 
 [Executioner]
   type = Transient
-  num_steps = 1
+  num_steps = 5
+[]
+
+[TallyGeneratorAction]
+    ids = '5 6 7'
+    nuclides = true
 []
 
 [Postprocessors]
@@ -20,6 +25,18 @@
   []
 []
 
+[Debug]
+  show_actions = true
+[]
+
 [Outputs]
   csv = true
+[]
+
+[Controls]
+ [webserver]
+   type = WebServerControl
+   execute_on = "TIMESTEP_BEGIN TIMESTEP_END"
+   port = 8000
+ []
 []
