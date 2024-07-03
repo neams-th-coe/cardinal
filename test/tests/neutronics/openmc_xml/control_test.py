582e07a6645e15fffea4cdea242b5a32ac9c8f5b
Patrick Shriwise <pshriwise@gmail.com>
2024-07-03 14:51:13 -0500

    Reverting changes to the openmc_xml test

--- test/tests/neutronics/openmc_xml/control_test.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-from MooseControl import MooseControl
-cmd='/home/pshriwise/soft/cardinal-dev/cardinal-opt -i openmc.i server.i --n-threads=10'
-control = MooseControl(cmd.split(), moose_control_name='webserver')
-control.initialize()
