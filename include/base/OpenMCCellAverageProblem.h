b16c4325d90cff92439925e0cc947b960190cb13
Patrick Shriwise <pshriwise@gmail.com>
2025-01-01 12:18:12 -0600

    Rebase fix

--- include/base/OpenMCCellAverageProblem.h
+++ include/base/OpenMCCellAverageProblem.h
@@ -721,8 +721,6 @@ protected:
    */
   virtual std::vector<int32_t> getMappedTallyIDs() const override;
 
-  std::unique_ptr<NumericVector<Number>> _serialized_solution;
-
   /**
    * Whether to automatically compute the mapping of OpenMC cell IDs and
    * instances to the [Mesh].
