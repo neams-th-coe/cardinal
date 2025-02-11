6ab61753ac84db252b525d45a9b2ed833f7c3e57
MendesMichael <154073752+MendesMichael@users.noreply.github.com>
2025-02-10 18:33:17 -0600

    Adding the header file

---
+++ include/postprocessors/Reactivity.h
@@ -0,0 +1,34 @@
+/********************************************************************/
+/*                  SOFTWARE COPYRIGHT NOTIFICATION                 */
+/*                             Cardinal                             */
+/*                                                                  */
+/*                  (c) 2021 UChicago Argonne, LLC                  */
+/*                        ALL RIGHTS RESERVED                       */
+/*                                                                  */
+/*                 Prepared by UChicago Argonne, LLC                */
+/*               Under Contract No. DE-AC02-06CH11357               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*             Prepared by Battelle Energy Alliance, LLC            */
+/*               Under Contract No. DE-AC07-05ID14517               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*                 See LICENSE for full restrictions                */
+/********************************************************************/
+
+#pragma once
+
+#include "OpenMCPostprocessor.h"
+#include "CardinalEnums.h"
+/**
+ * Calculate the reactivity based on the combined k-effective value.
+ */
+class Reactivity : public OpenMCPostprocessor
+{
+public:
+  static InputParameters validParams();
+
+  Reactivity(const InputParameters & parameters);
+
+  virtual Real getValue() const override;
+};
