0732a1eed8585036e5c79102632a91ab679a78ee
nuclearkevin <66632997+nuclearkevin@users.noreply.github.com>
2025-03-17 17:15:14 -0500

    Add a spherical harmonics filter to test changes. Ref. #1091

---
+++ include/filters/SphericalHarmonicsFilter.h
@@ -0,0 +1,44 @@
+/********************************************************************/
+/*                  SOFTWARE COPYRIGHT NOTIFICATION                 */
+/*                             Cardinal                             */
+/*                                                                  */
+/*                  (c) 2021 UChicago Argonne, LLC                  */
+/*                        ALL RIGHTS RESERVED                       */
+/*                                                                  */
+/*                 Prepared by UChicago Argonne, LLC                */
+/*               Under Contract No. DE-AC02-06CH11357               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*             Prepared by Battelle Energy Alliance, LLC            */
+/*               Under Contract No. DE-AC07-05ID14517               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*                 See LICENSE for full restrictions                */
+/********************************************************************/
+
+#pragma once
+
+#include "FilterBase.h"
+
+class SphericalHarmonicsFilter : public FilterBase
+{
+public:
+  static InputParameters validParams();
+
+  SphericalHarmonicsFilter(const InputParameters & parameters);
+
+  /// Override to return false on the first bin and true on all other bins.
+  virtual bool skipBin(const unsigned int bin) const override { return bin != 0; }
+
+  /**
+   * A function which returns the short-form name for each bin of
+   * this filter. Used to label auxvariables a TallyBase scores in.
+   * PolarAngleFilter(s) use 'theta' for each filter bin.
+   * @param[in] the bin index
+   * @return a short name for the bin represented by bin_index
+   */
+  virtual std::string binName(unsigned int bin_index) const override;
+protected:
+  /// The spherical harmonics order.
+  const unsigned int _order;
+};
---
+++ src/filters/SphericalHarmonicsFilter.C
@@ -0,0 +1,67 @@
+/********************************************************************/
+/*                  SOFTWARE COPYRIGHT NOTIFICATION                 */
+/*                             Cardinal                             */
+/*                                                                  */
+/*                  (c) 2021 UChicago Argonne, LLC                  */
+/*                        ALL RIGHTS RESERVED                       */
+/*                                                                  */
+/*                 Prepared by UChicago Argonne, LLC                */
+/*               Under Contract No. DE-AC02-06CH11357               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*             Prepared by Battelle Energy Alliance, LLC            */
+/*               Under Contract No. DE-AC07-05ID14517               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*                 See LICENSE for full restrictions                */
+/********************************************************************/
+
+#include "SphericalHarmonicsFilter.h"
+
+#include "openmc/tallies/filter_sph_harm.h"
+
+registerMooseObject("CardinalApp", SphericalHarmonicsFilter);
+
+InputParameters
+SphericalHarmonicsFilter::validParams()
+{
+  auto params = FilterBase::validParams();
+  params.addClassDescription("A class which provides a thin wrapper around an OpenMC SphericalHarmonicsFilter.");
+  params.addRequiredRangeCheckedParam<unsigned int>(
+    "order", "order <= 10", "The order of the spherical harmonics expansion. OpenMC only supports up to order 10.");
+
+  return params;
+}
+
+SphericalHarmonicsFilter::SphericalHarmonicsFilter(const InputParameters & parameters)
+  : FilterBase(parameters),
+    _order(getParam<unsigned int>("order"))
+{
+  auto sh_filter = dynamic_cast<openmc::SphericalHarmonicsFilter *>(openmc::Filter::create("sphericalharmonics"));
+  sh_filter->set_order(_order);
+  sh_filter->set_cosine("particle");
+  _filter = sh_filter;
+}
+
+std::string
+SphericalHarmonicsFilter::binName(unsigned int bin_index) const
+{
+  unsigned int num_mom = 0;
+  for (unsigned int l = 0; l <= _order; ++l)
+  {
+    for (int m = -1 * static_cast<int>(l); m < 0; ++m)
+    {
+      if (num_mom == bin_index)
+        return "l" + Moose::stringify(l) + "_mneg" + Moose::stringify(-1 * m);
+      num_mom++;
+    }
+    for (unsigned int m = 0; m <= l; ++m)
+    {
+      if (num_mom == bin_index)
+        return "l" + Moose::stringify(l) + "_mpos" + Moose::stringify(m);
+      num_mom++;
+    }
+  }
+
+  return "";
+}
