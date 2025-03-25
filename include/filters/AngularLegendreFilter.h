ad2499628327a72e63ad2ac44b4296d8f3fbd900
nuclearkevin <66632997+nuclearkevin@users.noreply.github.com>
2025-03-24 19:42:08 -0500

    Add a wrapping for Legendre filters in scattering angle. Ref. #1094

---
+++ include/filters/AngularLegendreFilter.h
@@ -0,0 +1,45 @@
+/********************************************************************/
+/*                  SOFTWARE COPYRIGHT NOTIFICATION                 */
+/*                             Cardinal                             */
+/*                                                                  */
+/*                  (c) 2021 UChicago Argonne, LLC                  */
+/*                        ALL RIGHTS RESERVED                       */
+/*                                                                  */
+/*                 Prepared by UChicago Argonne, LLC                */
+/*               Under Contract No. DE-AC02-06CH11357               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*             Prepared by Battelle Energy Alliance, LLC            */
+/*               Under Contract No. DE-AC07-05ID14517               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*                 See LICENSE for full restrictions                */
+/********************************************************************/
+
+#pragma once
+
+#include "FilterBase.h"
+
+class AngularLegendreFilter : public FilterBase
+{
+public:
+  static InputParameters validParams();
+
+  AngularLegendreFilter(const InputParameters & parameters);
+
+  /// Override to return false on the first bin and true on all other bins.
+  virtual bool skipBin(const unsigned int bin) const override { return bin != 0; }
+
+  /**
+   * A function which returns the short-form name for each bin of
+   * this filter. Used to label auxvariables a TallyBase scores in.
+   * AngularLegendreFilter(s) use l.
+   * @param[in] the bin index
+   * @return a short name for the bin represented by bin_index
+   */
+  virtual std::string binName(unsigned int bin_index) const override;
+
+private:
+  /// The Legendre order.
+  const unsigned int _order;
+};
---
+++ src/filters/AngularLegendreFilter.C
@@ -0,0 +1,54 @@
+/********************************************************************/
+/*                  SOFTWARE COPYRIGHT NOTIFICATION                 */
+/*                             Cardinal                             */
+/*                                                                  */
+/*                  (c) 2021 UChicago Argonne, LLC                  */
+/*                        ALL RIGHTS RESERVED                       */
+/*                                                                  */
+/*                 Prepared by UChicago Argonne, LLC                */
+/*               Under Contract No. DE-AC02-06CH11357               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*             Prepared by Battelle Energy Alliance, LLC            */
+/*               Under Contract No. DE-AC07-05ID14517               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*                 See LICENSE for full restrictions                */
+/********************************************************************/
+
+#ifdef ENABLE_OPENMC_COUPLING
+
+#include "AngularLegendreFilter.h"
+
+#include "openmc/tallies/filter_legendre.h"
+
+registerMooseObject("CardinalApp", AngularLegendreFilter);
+
+InputParameters
+AngularLegendreFilter::validParams()
+{
+  auto params = FilterBase::validParams();
+  params.addClassDescription(
+      "A class which provides a thin wrapper around an OpenMC LegendreFilter.");
+  params.addRequiredParam<unsigned int>("order", "The order of the Legendre expansion.");
+
+  return params;
+}
+
+AngularLegendreFilter::AngularLegendreFilter(const InputParameters & parameters)
+  : FilterBase(parameters), _order(getParam<unsigned int>("order"))
+{
+  auto legendre_filter = dynamic_cast<openmc::LegendreFilter *>(
+      openmc::Filter::create("legendre"));
+
+  legendre_filter->set_order(_order);
+  _filter = legendre_filter;
+}
+
+std::string
+AngularLegendreFilter::binName(unsigned int bin_index) const
+{
+  return "l" + Moose::stringify(bin_index + 1);
+}
+
+#endif
