c839da8cc78b15ef868a66f19512515cb9afadef
nuclearkevin <66632997+nuclearkevin@users.noreply.github.com>
2025-05-30 13:13:47 -0500

    Add Double-Down and Embree to Cardinal's build system. Also enables the use of Double-Down in DAGMC. Ref. #1129

---
+++ config/embree.mk
@@ -0,0 +1,31 @@
+$(EMBREE_BUILDDIR)/Makefile: $(EMBREE_DIR)/CMakeLists.txt
+	mkdir -p $(EMBREE_BUILDDIR)
+	cd $(EMBREE_BUILDDIR) && \
+	cmake -L \
+	-DCMAKE_BUILD_TYPE=$(BUILD_TYPE) \
+	-DCMAKE_C_COMPILER="$(LIBMESH_CC_LIST)" \
+	-DCMAKE_CXX_COMPILER="$(LIBMESH_CXX_LIST)" \
+	-DCMAKE_Fortran_COMPILER="$(LIBMESH_F90_LIST)" \
+	-DCMAKE_PREFIX_PATH=$(LIBMESH_DIR) \
+	-DCMAKE_INSTALL_PREFIX=$(EMBREE_INSTALL_DIR) \
+	-DCMAKE_INSTALL_LIBDIR=lib \
+	-DCMAKE_INSTALL_MESSAGE=LAZY \
+	-DEMBREE_TUTORIALS=OFF \
+	-DEMBREE_TUTORIALS_GLFW=OFF \
+	-DEMBREE_TASKING_SYSTEM=INTERNAL \
+	$(EMBREE_DIR)
+
+build_embree: | $(EMBREE_BUILDDIR)/Makefile
+	make VERBOSE=1 -C $(EMBREE_BUILDDIR) install
+
+cleanall_embree: | $(EMBREE_BUILDDIR)/Makefile
+	make -C $(EMBREE_BUILDDIR) clean
+
+clobber_embree:
+	rm -rf $(EMBREE_LIB) $(EMBREE_BUILDDIR) $(EMBREE_INSTALL_DIR)
+
+cleanall: cleanall_embree
+
+clobberall: clobber_embree
+
+.PHONY: build_embree cleanall_embree clobber_embree
