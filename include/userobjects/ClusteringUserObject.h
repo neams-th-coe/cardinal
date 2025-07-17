e3afcb7d6d16331b65127a614bf7f20e434e9ee8
magnoxemo <walid.ahammed01@gmail.com>
2025-07-17 10:40:21 -0500

    implementing new code design

--- include/userobjects/ClusteringUserObject.h
+++ include/userobjects/ClusteringUserObject.h
@@ -4,9 +4,7 @@
 
 class AuxiliarySystem;
 
-/*
- * Base class for clustering in cardinal.
- * */
+/* Base class for clustering in cardinal. */
 class ClusteringUserObject : public GeneralUserObject
 {
 
@@ -14,24 +12,11 @@ public:
   static InputParameters validParams();
   ClusteringUserObject(const InputParameters & parameters);
 
-  virtual void execute() override;
+  virtual void execute() override {};
   virtual void initialize() override {};
   virtual void finalize() override {};
 
-  ///Getter function for the extra element integer value of an element
-  int getExtraIntegerScore(libMesh::Elem * elem) const;
-
 protected:
-
-  ///Sets the extra element integer id of every active element in the mesh to NOT_VISITED
-  void applyNoClusteringInitialCondition();
-
-  /**
-   * Cluster finder method. It iterates thourgh the elements in the mesh.
-   * if two elements belongs to a cluster it changes the extra element integer of those two elements to a similar value.
-   */
-  void findCluster();
-
   ///Get the metric data from the auxiliary system for an element.
   Real getMetricData(const libMesh::Elem * elem) const;
 
@@ -39,10 +24,7 @@ protected:
    * A purely virtual fucntion which must be overrided in derived classes.
    * It applies the clustering logic for two elements in the derived class
    */
-  virtual bool belongsToCluster(libMesh::Elem * base_element, libMesh::Elem * neighbor_elem) = 0;
-
-  ///Holds the ExtraElementIDName
-  const ExtraElementIDName _id_name;
+  virtual bool belongsToCluster(libMesh::Elem * base_element, libMesh::Elem * neighbor_elem) const = 0;
 
   ///Mesh reference
   libMesh::MeshBase & _mesh;
@@ -61,14 +43,4 @@ protected:
 
   ///Metric variable index
   unsigned int _metric_variable_index;
-
-  ///Extra element id index
-  unsigned int _extra_integer_index = 0;
-
-  /**
-   * If extra element integer value is equal to NOT_VISITED  that indicates
-   * an element has not yet been visited or assigned to a cluster while findCluster
-   * method is iterating through the elements in the mesh.
-   */
-  static constexpr int NOT_VISITED = -1;
 };
--- src/userobjects/ClusteringUserObject.C
+++ src/userobjects/ClusteringUserObject.C
@@ -5,7 +5,6 @@ InputParameters
 ClusteringUserObject::validParams()
 {
   InputParameters params = GeneralUserObject::validParams();
-  params.addRequiredParam<ExtraElementIDName>("id_name", "extra_element_integer_id name");
   params.addRequiredParam<AuxVariableName>(
       "metric_variable_name", "The name of the variable based on which clustering will be done");
 
@@ -14,7 +13,6 @@ ClusteringUserObject::validParams()
 
 ClusteringUserObject::ClusteringUserObject(const InputParameters & parameters)
   : GeneralUserObject(parameters),
-    _id_name(getParam<ExtraElementIDName>("id_name")),
     _mesh(_fe_problem.mesh().getMesh()),
     _metric_variable_name(getParam<AuxVariableName>("metric_variable_name")),
     _metric_variable(_fe_problem.getVariable(_tid, _metric_variable_name)),
@@ -22,29 +20,11 @@ ClusteringUserObject::ClusteringUserObject(const InputParameters & parameters)
     _dof_map(_auxiliary_system.dofMap()),
     _metric_variable_index(_auxiliary_system.getVariable(_tid, _metric_variable_name).number())
 {
-  if (!_mesh.has_elem_integer(_id_name))
-  {
-    mooseWarning("Mesh does not have an extra element integer named ",
-                 _id_name,
-                 "."
-                 " so adding the ",
-                 _id_name,
-                 " generator defines it with extra_element_integers.");
-    _mesh.add_elem_integer(_id_name);
-  }
   //check if the element type if CONSTANT MONOMIAL. If not then throw a mooseError.
   if (_metric_variable.feType()!=FEType(CONSTANT, MONOMIAL))
     mooseError("Variable must be type of CONSTANT MONOMIAL");
-  _extra_integer_index = _mesh.get_elem_integer_index(_id_name);
 }
 
-void
-ClusteringUserObject::execute()
-{
-  //set the extra element integer value to NOT_VISITED
-  applyNoClusteringInitialCondition();
-  findCluster();
-}
 
 Real
 ClusteringUserObject::getMetricData(const libMesh::Elem * elem) const
@@ -56,63 +36,4 @@ ClusteringUserObject::getMetricData(const libMesh::Elem * elem) const
   _auxiliary_system.solution().get(dof_indices, solution_value);
 
   return solution_value[0];
-}
-
-void
-ClusteringUserObject::applyNoClusteringInitialCondition()
-{
-  for (auto & elem : _mesh.active_element_ptr_range())
-    elem->set_extra_integer(_extra_integer_index, NOT_VISITED);
-}
-
-void
-ClusteringUserObject::findCluster()
-{
-
-  std::stack<libMesh::Elem *> neighbor_stack;
-
-  for (auto & elem : _mesh.active_element_ptr_range())
-  {
-    //if elem->get_extra_integer(_extra_integer_index) != NOT_VISITED that means we have already touched that element.
-    // No need to visit it again.
-    if (elem->get_extra_integer(_extra_integer_index) != NOT_VISITED)
-      continue;
-
-    int cluster_id = elem->id();
-    neighbor_stack.push(elem);
-
-    while (!neighbor_stack.empty())
-    {
-      libMesh::Elem * current_elem = neighbor_stack.top();
-      neighbor_stack.pop();
-
-      //Iterating through the side as, two elements to be clustered together they must share one side or face.
-      for (unsigned int s = 0; s < current_elem->n_sides(); s++)
-      {
-        libMesh::Elem * neighbor_elem = current_elem->neighbor_ptr(s);
-        //check if the neighbor_elem is
-        //1. not a null ptr
-        //2. an active element
-        //3. NOT_VISITED before
-        if (neighbor_elem && neighbor_elem->active() &&
-            neighbor_elem->get_extra_integer(_extra_integer_index) == NOT_VISITED)
-        {
-          if (belongsToCluster(current_elem, neighbor_elem))
-          {
-            //if elements belong to a cluster then change the extra element integer
-            //of both elements to the element id of the current element
-            elem->set_extra_integer(_extra_integer_index, cluster_id);
-            neighbor_elem->set_extra_integer(_extra_integer_index, cluster_id);
-            neighbor_stack.push(neighbor_elem);
-          }
-        }
-      }
-    }
-  }
-}
-
-int
-ClusteringUserObject::getExtraIntegerScore(libMesh::Elem * elem) const
-{
-  return elem->get_extra_integer(_extra_integer_index);
 }
