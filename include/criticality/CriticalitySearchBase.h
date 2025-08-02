efcb7cdcfb9a239114dd3c17e413543bf2c75a3c
April Novak <novak@berkeley.edu>
2025-08-01 22:50:20 -0500

    implement criticality search for density. Refs #1158

--- include/criticality/CriticalitySearchBase.h
+++ include/criticality/CriticalitySearchBase.h
@@ -19,25 +19,54 @@
 #pragma once
 
 #include "MooseObject.h"
-#include "OpenMCCellAverageProblem.h"
+#include "OpenMCBase.h"
+#include "CardinalEnums.h"
 
-class CriticalitySearchBase : public MooseObject
+/**
+ * Applies a criticality search for each OpenMC solve. The critical configuration
+ * is then used for the multiphysics coupling.
+ */
+class CriticalitySearchBase : public MooseObject, public OpenMCBase
 {
 public:
   static InputParameters validParams();
 
   CriticalitySearchBase(const InputParameters & parameters);
 
+  /**
+   * Modify the OpenMC model
+   * @param[in] input value to apply to the new model; interpretation depends on derived classes
+   */
   virtual void updateOpenMCModel(const Real & input) = 0;
 
+  /// Use Brent's method to search for criticality
   virtual void searchForCriticality();
 
 protected:
+  /// The quantity being varied in the search for criticality, for console prints
   virtual std::string quantity() const = 0;
 
+  /// Assumed units in the input quantities
+  virtual std::string units() const = 0;
+
+  /// Maximum range of value to explore
   const Real & _maximum;
+
+  /// Minimum range of value to explore
   const Real & _minimum;
+
+  /// Absolute tolerance for finding a critical configuration
   const Real & _tolerance;
 
-  OpenMCCellAverageProblem & _openmc_problem;
+  /// Estimator to use for k
+  const eigenvalue::EigenvalueEnum _estimator;
+
+  /// Values used in search
+  std::vector<Real> _inputs;
+
+  /// Values obtained in search
+  std::vector<Real> _k_values;
+
+  /// Standard deviation values obtained in search
+  std::vector<Real> _k_std_dev_values;
 };
