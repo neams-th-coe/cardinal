d58f42c1c038720842c3347a2b283d7abc725543
April Novak <novak@berkeley.edu>
2025-08-04 12:01:26 -0500

    incorporate review feedback

--- src/actions/AddCriticalitySearchAction.C
+++ src/actions/AddCriticalitySearchAction.C
@@ -48,7 +48,6 @@ AddCriticalitySearchAction::act()
       mooseError("The [CriticalitySearch] block can only be used with wrapped OpenMC cases! "
                  "You need to change the [Problem] block to 'OpenMCCellAverageProblem'.");
 
-    _moose_object_pars.set<OpenMCCellAverageProblem *>("_openmc_problem") = openmc_problem;
     auto search = openmc_problem->addObject<CriticalitySearchBase>(
         _type, _name, _moose_object_pars, false)[0];
   }
