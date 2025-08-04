f40ee660862b91a293ef1707698aafb4f7e6e0cd
April Novak <novak@berkeley.edu>
2025-08-04 15:43:39 -0500

    add criticality search for boron. Refs #1158

--- test/tests/criticality/borated_water/make_openmc_model.py
+++ test/tests/criticality/borated_water/make_openmc_model.py
@@ -21,23 +21,28 @@ import openmc
 model = openmc.Model()
 
 u = openmc.Material()
-u.set_density('g/cc', 10.0)
-u.add_element('U', 1.0)
-u.add_nuclide('H3', 1.0)
+u.set_density('g/cc', 22.0)
+u.add_nuclide('U234', 0.001)
+u.add_nuclide('U235', 0.1)
+u.add_nuclide('U238', 1.0)
+u.add_nuclide('H3', 1e-4)
 
-bw = openmc.model.borated_water(boron_ppm=1000, density=0.8)
+bw = openmc.model.borated_water(boron_ppm=3000, density=0.8)
+h1_percent = bw.nuclides[0].percent
+for n in bw.nuclides:
+  print(n.name, n.percent / h1_percent)
 
 model.materials = openmc.Materials([u, bw])
 
 # Create two cubes side by side
 R = 10.0
-l = openmc.XPlane(x0=0, boundary_type='vacuum')
+l = openmc.XPlane(x0=0, boundary_type='reflective')
 r = openmc.XPlane(x0=R)
-b = openmc.YPlane(y0=0, boundary_type='vacuum')
-t = openmc.YPlane(y0=R, boundary_type='vacuum')
-f = openmc.ZPlane(z0=0, boundary_type='vacuum')
-k = openmc.ZPlane(z0=R, boundary_type='vacuum')
-z = openmc.XPlane(x0=2*R, boundary_type='vacuum')
+b = openmc.YPlane(y0=0, boundary_type='reflective')
+t = openmc.YPlane(y0=R, boundary_type='reflective')
+f = openmc.ZPlane(z0=0, boundary_type='reflective')
+k = openmc.ZPlane(z0=R, boundary_type='reflective')
+z = openmc.XPlane(x0=2*R, boundary_type='reflective')
 
 # create a box
 box1 = openmc.Cell(region=+l & -r & +b & -t & +f & -k, fill=u)
@@ -47,7 +52,7 @@ model.geometry = openmc.Geometry([box1, box2])
 # Finally, define some run settings
 model.settings.batches = 50
 model.settings.inactive = 10
-model.settings.particles = 100
+model.settings.particles = 1000
 
 lower_left = (0, 0, 0)
 upper_right = (R, R, R)
--- test/tests/criticality/borated_water/openmc.i
+++ test/tests/criticality/borated_water/openmc.i
@@ -3,23 +3,44 @@
     type = GeneratedMeshGenerator
     dim = 3
     xmin = 0
-    xmax = 10
+    xmax = 20
     ymin = 0
     ymax = 10
     zmin = 0
     zmax = 10
+    nx = 10
+    ny = 1
+    nz = 1
+  []
+  [split_in_half]
+    type = ParsedSubdomainMeshGenerator
+    input = g
+    combinatorial_geometry = 'x > 10'
+    block_id = '2'
+  []
+[]
+
+[ICs]
+  [density]
+    type = ConstantIC
+    variable = density
+    value = 1900.0
   []
 []
 
 [Problem]
   type = OpenMCCellAverageProblem
+  density_blocks = '2'
+  cell_level = 0
+  verbose = true
 
   [CriticalitySearch]
     type = BoratedWater
+    absent_nuclides = 'O18'
     material_id = 2
-    minimum = 100
-    maximum = 3000
-    tolerance = 1e-2
+    minimum = 0
+    maximum = 1000
+    tolerance = 5e-2
   []
 []
 
