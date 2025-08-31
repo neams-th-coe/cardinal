c68763e1ac2b2a63b3bfff431590d6a2635b2e26
nuclearkevin <66632997+nuclearkevin@users.noreply.github.com>
2025-08-31 16:31:54 -0500

    Use improved cell density API. - Avoid directly setting density multipliers.

--- src/auxkernels/CellDensityAux.C
+++ src/auxkernels/CellDensityAux.C
@@ -57,23 +57,16 @@ CellDensityAux::computeValue()
   if (_openmc_problem->materialID(index) == -1)
     return OpenMCCellAverageProblem::UNMAPPED;
 
+  // Fetch the density.
   double density;
-  int err = openmc_material_get_density(index, &density);
+  int err = openmc_cell_get_density(cell_info.first, &cell_info.second, &density);
 
   if (err)
-    mooseError("In attempting to get density for " + _openmc_problem->printMaterial(index) +
-               ", OpenMC reported:\n\n" + std::string(openmc_err_msg));
-
-  // Fetch the density multiplier.
-  double density_mult;
-  err = openmc_cell_get_density_mult(cell_info.first, &cell_info.second, &density_mult);
-
-  if (err)
-    mooseError("In attempting to get the density multiplier for " +
+    mooseError("In attempting to get the density for " +
                _openmc_problem->printCell(cell_info) + ", OpenMC reported:\n\n" +
                std::string(openmc_err_msg));
 
-  return density_mult * density / _openmc_problem->densityConversionFactor();
+  return density / _openmc_problem->densityConversionFactor();
 }
 
 #endif
