86e4c0dad6d8187513ada77530a0b60feec2fff7
nuclearkevin <66632997+nuclearkevin@users.noreply.github.com>
2025-09-12 09:43:25 -0500

    Address April's review. Ref. #1207

--- include/filters/AzimuthalAngleFilter.h
+++ include/filters/AzimuthalAngleFilter.h
@@ -20,8 +20,10 @@
 
 #include "FilterBase.h"
 
-/// A class which provides a thin wrapper around an OpenMC AzimuthalFilter
-/// for use by Cardinal mapped tallies.
+/**
+ * A class which provides a thin wrapper around an OpenMC AzimuthalFilter
+ * for use by Cardinal mapped tallies.
+ */
 class AzimuthalAngleFilter : public FilterBase
 {
 public:
