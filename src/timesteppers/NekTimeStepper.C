7c7d1c4633403dd92f66cb2fbf0ba1948d06ccb6
Nadish Saini <nsaini.ne@gmail.com>
2025-12-04 21:05:32 -0600

    resolved NekTimeStepper errors

--- src/timesteppers/NekTimeStepper.C
+++ src/timesteppers/NekTimeStepper.C
@@ -80,7 +80,8 @@ NekTimeStepper::NekTimeStepper(const InputParameters & parameters)
                  "already specified by 'NekTimeStepper'!");
 
   // at this point, this is non-dimensional dt
-  _nek_dt = nekrs::dt(1);
+  auto [dtSubStep, dt] = nekrs::dt(1); 
+  _nek_dt = dt;
 }
 
 Real
@@ -92,7 +93,8 @@ NekTimeStepper::computeInitialDT()
 Real
 NekTimeStepper::computeDT()
 {
-  Real dt = nekrs::hasVariableDt() ? nekrs::dt(_t_step) : _nek_dt;
+  auto [dtSubStep, _dt] = nekrs::dt(_t_step); 
+  Real dt = nekrs::hasVariableDt() ? _dt : _nek_dt;
   return dt * nekrs::referenceTime();
 }
 
