7a392c296f0ead80aeae862260e49fb8eb8889de
Mahmoud Eltawila <mahmoud.eltawila@gmail.com>
2025-12-09 17:44:02 -0600

    formatting

--- src/base/OpenMCCellAverageProblem.C
+++ src/base/OpenMCCellAverageProblem.C
@@ -2304,8 +2304,8 @@ OpenMCCellAverageProblem::externalSolve()
     if (isParamValid("particles"))
       openmc::settings::n_particles = OpenMCProblemBase::nParticles();
   }
-  _console << " Running OpenMC with " << openmc::settings::n_particles
-             << " particles per batch..." << std::endl;
+  _console << " Running OpenMC with " << openmc::settings::n_particles << " particles per batch..."
+           << std::endl;
 
   OpenMCProblemBase::externalSolve();
 }
