ce2efa58ed8a25bf9fe755ccaf2eb4a66f3056e6
Mahmoud Eltawila <mahmoud.eltawila@gmail.com>
2026-02-18 09:32:04 -0600

    formatting

--- src/base/OpenMCCellAverageProblem.C
+++ src/base/OpenMCCellAverageProblem.C
@@ -523,7 +523,8 @@ OpenMCCellAverageProblem::initialSetup()
   getOpenMCUserObjects();
 
   if (_use_displaced && !_using_skinner && !hasCellTransform())
-    mooseWarning("Your problem has a moving mesh, but you have not provided a 'skinner' or an OpenMCCellTransform user object (both of which move the OpenMC geometry). The "
+    mooseWarning("Your problem has a moving mesh, but you have not provided a 'skinner' or an "
+                 "OpenMCCellTransform user object (both of which move the OpenMC geometry). The "
                  "[Mesh] will move, but the underlying OpenMC geometry will remain unchanged. "
                  "Unexpected behavior may occur.");
 
