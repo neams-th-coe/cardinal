c06b295fc2dcbb037093293bc6b0c1aa7d635f98
Joffrey Dorville <jdorville@anl.gov>
2026-06-11 14:54:47 -0500

    Add OpenMCMeshGenerator

---
+++ include/meshgenerators/OpenMCMeshGenerator.h
@@ -0,0 +1,51 @@
+/********************************************************************/
+/*                  SOFTWARE COPYRIGHT NOTIFICATION                 */
+/*                             Cardinal                             */
+/*                                                                  */
+/*                  (c) 2021 UChicago Argonne, LLC                  */
+/*                        ALL RIGHTS RESERVED                       */
+/*                                                                  */
+/*                 Prepared by UChicago Argonne, LLC                */
+/*               Under Contract No. DE-AC02-06CH11357               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*             Prepared by Battelle Energy Alliance, LLC            */
+/*               Under Contract No. DE-AC07-05ID14517               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*                 See LICENSE for full restrictions                */
+/********************************************************************/
+
+#pragma once
+
+#include "MeshGenerator.h"
+
+#include "openmc/mesh.h"
+
+/*
+ * MeshGenerator that uses an existing OpenMC mesh to create an equivalent one with libmesh.
+ */
+class OpenMCMeshGenerator : public MeshGenerator
+{
+public:
+  static InputParameters validParams();
+
+  OpenMCMeshGenerator(const InputParameters & parameters);
+
+  std::unique_ptr<MeshBase> generate() override;
+
+private:
+  /// Generated mesh
+  std::unique_ptr<MeshBase> * _build_mesh;
+
+  const unsigned int _mesh_id;
+
+  const Real & _scaling;
+
+  openmc::Mesh * _mesh = nullptr;
+
+  unsigned int _mesh_index;
+
+  /// Directory in which OpenMC settings xml files are located
+  const std::string & _xml_directory;
+};
---
+++ src/meshgenerators/OpenMCMeshGenerator.C
@@ -0,0 +1,99 @@
+/********************************************************************/
+/*                  SOFTWARE COPYRIGHT NOTIFICATION                 */
+/*                             Cardinal                             */
+/*                                                                  */
+/*                  (c) 2021 UChicago Argonne, LLC                  */
+/*                        ALL RIGHTS RESERVED                       */
+/*                                                                  */
+/*                 Prepared by UChicago Argonne, LLC                */
+/*               Under Contract No. DE-AC02-06CH11357               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*             Prepared by Battelle Energy Alliance, LLC            */
+/*               Under Contract No. DE-AC07-05ID14517               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*                 See LICENSE for full restrictions                */
+/********************************************************************/
+
+#ifdef ENABLE_OPENMC_COUPLING
+
+#include "OpenMCMeshGenerator.h"
+#include "GeneratedMeshGenerator.h"
+
+registerMooseObject("CardinalApp", OpenMCMeshGenerator);
+
+InputParameters
+OpenMCMeshGenerator::validParams()
+{
+  InputParameters params = MeshGenerator::validParams();
+
+  params.addClassDescription(
+      "A class which provides a thin wrapper around an arbitrary OpenMC mesh.");
+  params.addRequiredParam<unsigned int>(
+      "mesh_id", "The id of the OpenMC mesh that this class should provide to Cardinal.");
+  params.addRangeCheckedParam<Real>(
+      "scaling",
+      1.0,
+      "scaling > 0.0",
+      "Scaling factor to apply to [Mesh] to get to units of centimeters that OpenMC expects; "
+      "setting 'scaling = 100.0', for instance, indicates that the [Mesh] is in units of meters");
+  params.addParam<FileName>(
+      "xml_directory", "./", "The directory in which to look for OpenMC XML files.");
+  return params;
+}
+
+OpenMCMeshGenerator::OpenMCMeshGenerator(const InputParameters & params)
+  : MeshGenerator(params),
+    _mesh_id(getParam<unsigned int>("mesh_id")),
+    _scaling(getParam<Real>("scaling")),
+    _xml_directory(getParam<FileName>("xml_directory"))
+{
+  // Check to make sure the mesh exists.
+  if (openmc::model::mesh_map.count(_mesh_id) == 0)
+    paramError("mesh_id",
+               "A mesh with the id " + Moose::stringify(_mesh_id) +
+                   " does not exist in the OpenMC model! Please make sure the mesh has been "
+                   "added in the OpenMC model and you've supplied the correct mesh id.");
+
+  _mesh_index = openmc::model::mesh_map.at(_mesh_id);
+  _mesh = openmc::model::meshes[_mesh_index].get();
+
+  if (_mesh->n_dimension_ != 3)
+  {
+    mooseError("3D mesh only!");
+  }
+
+  // Construct mesh using a subgenerator
+  if (_mesh->get_mesh_type() == openmc::RegularMesh::mesh_type)
+  {
+    openmc::RegularMesh * regular_mesh = dynamic_cast<openmc::RegularMesh *>(_mesh);
+
+    InputParameters params = GeneratedMeshGenerator::validParams();
+    params.set<MooseEnum>("dim") = regular_mesh->n_dimension_;
+    params.set<unsigned int>("nx") = regular_mesh->shape_[0];
+    params.set<unsigned int>("ny") = regular_mesh->shape_[1];
+    params.set<unsigned int>("nz") = regular_mesh->shape_[2];
+    params.set<Real>("xmin") = regular_mesh->lower_left()[0] / _scaling;
+    params.set<Real>("xmax") = regular_mesh->upper_right()[0] / _scaling;
+    params.set<Real>("ymin") = regular_mesh->lower_left()[1] / _scaling;
+    params.set<Real>("ymax") = regular_mesh->upper_right()[1] / _scaling;
+    params.set<Real>("zmin") = regular_mesh->lower_left()[2] / _scaling;
+    params.set<Real>("zmax") = regular_mesh->upper_right()[2] / _scaling;
+    addMeshSubgenerator("GeneratedMeshGenerator", name() + "_openmc_sub_mesh", params);
+
+    _build_mesh = &getMeshByName(name() + "_openmc_sub_mesh");
+  }
+  else
+  {
+    mooseError("Only implemented for openmc::RegularMesh!");
+  }
+}
+
+std::unique_ptr<MeshBase>
+OpenMCMeshGenerator::generate()
+{
+  return std::move(*_build_mesh);
+}
+
+#endif
