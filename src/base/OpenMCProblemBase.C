80dbb7e635e60694231f10a767b1b7eca501215e
nuclearkevin <66632997+nuclearkevin@users.noreply.github.com>
2026-06-26 12:25:46 -0500

    Clang-format loves being picky. Ref. #1273

--- src/base/OpenMCProblemBase.C
+++ src/base/OpenMCProblemBase.C
@@ -180,7 +180,7 @@ OpenMCProblemBase::OpenMCProblemBase(const InputParameters & params)
 
       if (!runRandomRay())
         checkUnusedParam(
-          params, "inactive_batches", "running in fixed source mode with the Monte Carlo solver");
+            params, "inactive_batches", "running in fixed source mode with the Monte Carlo solver");
       checkUnusedParam(params, "reuse_source", "running in fixed source mode");
       checkUnusedParam(params, "power", "running in fixed source mode");
       _reuse_source = false;
