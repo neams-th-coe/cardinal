2bceedfafcc51fcae9ba4b3b5cb2936adf2a91bb
Joffrey Dorville <jdorville@anl.gov>
2026-08-05 15:55:01 -0500

    Formatting

--- src/actions/OpenMCInitAction.C
+++ src/actions/OpenMCInitAction.C
@@ -60,12 +60,14 @@ OpenMCInitAction::act()
     // Check for xml_directory consistency
     if (xml_directory_problem != xml_directory_generator)
       mooseError("Inconsistent 'xml_directory' for OpenMC in the mesh generator and problem "
-                 "declarations. Make sure the same value is used throughout the [Mesh] and [Problem] blocks.");
+                 "declarations. Make sure the same value is used throughout the [Mesh] and "
+                 "[Problem] blocks.");
 
     // Check for scaling consistency
     if (scaling_problem != scaling_generator)
       mooseError("Inconsistent 'scaling' factors for OpenMC in the mesh generator and problem "
-                 "declarations. Make sure the same value is used throughout the [Mesh] and [Problem] blocks.");
+                 "declarations. Make sure the same value is used throughout the [Mesh] and "
+                 "[Problem] blocks.");
   }
 
   // Select xml_directory
@@ -114,13 +116,15 @@ OpenMCInitAction::isOpenMCMeshGeneratorRequested(std::string & xml_directory, Re
       {
         // Check for xml_directory consistency
         if (xml_directory_temp != xml_directory)
-          mooseError(
-              "Inconsistent 'xml_directory' values found in the declared OpenMCMeshGenerators; the same choice for 'xml_directory' must be used across all OpenMCMeshGenerator objects");
+          mooseError("Inconsistent 'xml_directory' values found in the declared "
+                     "OpenMCMeshGenerators; the same choice for 'xml_directory' must be used "
+                     "across all OpenMCMeshGenerator objects");
 
         // Check for scaling consistency
         if (scaling_temp != scaling)
           mooseError(
-              "Inconsistent 'scaling' factors for OpenMC in the declared OpenMCMeshGenerators; the same choice for 'scaling' must be used across all OpenMCMeshGenerator objects");
+              "Inconsistent 'scaling' factors for OpenMC in the declared OpenMCMeshGenerators; the "
+              "same choice for 'scaling' must be used across all OpenMCMeshGenerator objects");
       }
       else
       {
--- src/meshgenerators/OpenMCMeshGenerator.C
+++ src/meshgenerators/OpenMCMeshGenerator.C
@@ -36,7 +36,9 @@ OpenMCMeshGenerator::validParams()
       "scaling",
       1.0,
       "scaling > 0.0",
-      "Scaling factor used to scale the OpenMC mesh dimensions for the creation of the mesh; OpenMC always uses units of centimeters. Setting 'scaling = 100' means that the [Mesh] created will have units of meters, for example.");
+      "Scaling factor used to scale the OpenMC mesh dimensions for the creation of the mesh; "
+      "OpenMC always uses units of centimeters. Setting 'scaling = 100' means that the [Mesh] "
+      "created will have units of meters, for example.");
   params.addParam<FileName>(
       "xml_directory", "./", "The directory in which to look for OpenMC XML files.");
   return params;
@@ -83,7 +85,9 @@ OpenMCMeshGenerator::OpenMCMeshGenerator(const InputParameters & params)
     _build_mesh = &getMeshByName(name() + "_openmc_sub_mesh");
   }
   else
-    mooseError("This object is currently only implemented for regular meshes (RegularMesh) in OpenMC! Please contact the Cardinal developer team if you require another structured mesh type.");
+    mooseError(
+        "This object is currently only implemented for regular meshes (RegularMesh) in OpenMC! "
+        "Please contact the Cardinal developer team if you require another structured mesh type.");
 }
 
 std::unique_ptr<MeshBase>
