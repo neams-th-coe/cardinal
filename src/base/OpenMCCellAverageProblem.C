b817a115da03f1ba8b4be0f1199e3f4d10028596
Andrew Davis <andrew.davis@ukaea.uk>
2026-09-14 20:23:13 +0100

    Changed errors to warnings

--- src/base/OpenMCCellAverageProblem.C
+++ src/base/OpenMCCellAverageProblem.C
@@ -695,23 +695,23 @@ OpenMCCellAverageProblem::initialSetup()
 
     for (const auto & [temp_var, blocks] : _temp_vars_to_blocks)
       if (!_skinner->binsByField(temp_var))
-        mooseError("Detected inconsistent settings for temperature feedback and the '",
+        mooseWarning("Potentially inconsistent settings for temperature feedback found in the '",
                    name,
                    "' skinner. Temperature feedback is applied with variable '",
                    temp_var,
-                   "' (via 'temperature_blocks'), which requires the skinner to bin by that "
-                   "variable. Please add '",
+                   "' (via 'temperature_blocks'), typically the skinner should bin by that "
+                   "variable. You may wish to add '",
                    temp_var,
-                   "' to the skinner's 'fields' parameter.");
+		     "' to the skinner's 'fields' parameter.");
 
     for (const auto & [density_var, blocks] : _density_vars_to_blocks)
       if (!_skinner->binsByField(density_var))
-        mooseError("Detected inconsistent settings for density feedback and the '",
+        mooseWarning("Potentially inconsistent settings for density feedback found in the '",
                    name,
                    "' skinner. Density feedback is applied with variable '",
                    density_var,
-                   "' (via 'density_blocks'), which requires the skinner to bin by that "
-                   "variable. Please add '",
+                   "' (via 'density_blocks'), typically the skinner should bin by that "
+                   "variable. You may wish to add '",
                    density_var,
                    "' to the skinner's 'fields' parameter.");
 
