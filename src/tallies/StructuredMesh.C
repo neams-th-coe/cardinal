27fbf52f4d565f7c2c97337f95a1cb7cc1108865
Andrew Davis <andrew.davis@ukaea.uk>
2026-09-18 10:24:36 +0100

    Misnamed header include file

--- src/tallies/StructuredMesh.C
+++ src/tallies/StructuredMesh.C
@@ -21,7 +21,7 @@
 #include "MooseError.h"
 #include "libmesh/edge_edge2.h"
 #include "libmesh/elem.h"
-#include "libmesh/hex8.h"
+#include "libmesh/cell_hex8.h"
 #include "libmesh/mesh_base.h"
 #include "libmesh/parallel.h"
 #include "libmesh/quad4.h"
